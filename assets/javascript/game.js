cc639a5548337da2f7d71ee3c1e956b4ea73b01b
Guillaume Paugam <Guillaume@Guillaumes-MacBook-Air.local>
2018-03-01 17:45:17 -0600

    Working Keyboard Check

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -335,6 +335,9 @@ const cssZindex = {name: "z-index", type: "CSS", example: "N/A", definition: "Se
 const htmlArray =[a, abbr, address, area, article, aside, audio, b, base, bdi, bdo, blockquote, body, br, button, canvas, caption, cite, code, col, colgroup, dd, datalist, del, details, dfn, dialog, div, dl, dt, em, embed, fieldset, figcaption, figure, footer, form, head, header, hr, html, i, iframe, img, input, ins, kbd, label, legend, li, link, main, map, mark, menu, menuitem, meta, meter, nav, noscript, object, ol, optgroup, option, output, p, param, picture, pre, progress, q, rp, rt, ruby, s, samp, script, section, select, small, source, span, strong, style, sub, summary, sup, table, tbody, td, template, textarea, tfoot, th, thead, time, title, tr, track, u, ul, video, wbr];
 const cssArray = [cssCharset, cssFontface, cssFontfeaturevalues, cssImport, cssKeyframes, cssMedia, cssAligncontent, cssAlignitems, cssAlignself, cssAll, cssAnimation, cssAnimationdelay, cssAnimationdirection, cssAnimationduration, cssAnimationfillmode, cssAnimationiterationcount, cssAnimationname, cssAnimationplaystate, cssAnimationtimingfunction, cssBackfacevisibility, cssBackground, cssBackgroundblendmode, cssBackgroundattachment, cssBackgroundclip, cssBackgroundcolor, cssBackgroundimage, cssBackgroundorigin, cssBackgroundposition, cssBackgroundrepeat, cssBackgroundsize, cssBorder, cssBorderbottom, cssBorderbottomcolor, cssBorderbottomleftradius, cssBorderbottomrightradius, cssBorderbottomstyle, cssBorderbottomwidth, cssBordercollapse, cssBordercolor, cssBorderimage, cssBorderimageoutset, cssBorderimagerepeat, cssBorderimageslice, cssBorderimagesource, cssBorderimagewidth, cssBorderleft, cssBorderleftcolor, cssBorderleftstyle, cssBorderleftwidth, cssBorderradius, cssBorderright, cssBorderrightcolor, cssBorderrightstyle, cssBorderrightwidth, cssBorderspacing, cssBorderstyle, cssBordertop, cssBordertopcolor, cssBordertopleftradius, cssBordertoprightradius, cssBordertopstyle, cssBordertopwidth, cssBorderwidth, cssBottom, cssBoxdecorationbreak, cssBoxshadow, cssBoxsizing, cssBreakafter, cssBreakbefore, cssBreakinside, cssCaptionside, cssCaretcolor, cssClear, cssClip, cssColor, cssColumncount, cssColumnfill, cssColumngap, cssColumnrule, cssColumnrulecolor, cssColumnrulestyle, cssColumnrulewidth, cssColumnspan, cssColumnwidth, cssColumns, cssContent, cssCounterincrement, cssCounterreset, cssCursor, cssDirection, cssDisplay, cssEmptycells, cssFilter, cssFlex, cssFlexbasis, cssFlexdirection, cssFlexflow, cssFlexgrow, cssFlexshrink, cssFlexwrap, cssFloat, cssFont, cssFontfamily, cssFontfeaturesettings,cssFontkerning, cssFontlanguageoverride, cssFontsize, cssFontsizeadjust, cssFontstretch, cssFontstyle, cssFontsynthesis, cssFontvariant, cssFontvariantalternates, cssFontvariantcaps, cssFontvarianteastasian, cssFontvariantligatures, cssFontvariantnumeric, cssFontvariantposition, cssFontweight, cssGrid, cssGridarea, cssGridautocolumns, cssGridautoflow, cssGridautorows, cssGridcolumn, cssGridcolumnend, cssGridcolumngap, cssGridcolumnstart, cssGridgap, cssGridrow, cssGridrowend, cssGridrowgap, cssGridrowstart, cssGridtemplate, cssGridtemplateareas, cssGridtemplatecolumns, cssGridtemplaterows, cssHangingpunctuation, cssHeight, cssHyphens, cssImageorientation, cssImagerendering, cssImageresolution, cssJustifycontent, cssLeft, cssLetterspacing, cssLinebreak, cssLineheight, cssListstyle, cssListstyleimage, cssListstyleposition, cssListstyletype, cssMargin, cssMarginbottom, cssMarginright, cssMarginleft, cssMargintop, cssMaxheight, cssMaxwidth, cssMinheight, cssMinwidth, cssObjectfit, cssObjectposition, cssOpacity, cssOrder, cssOrphans, cssOutline, cssOutlinecolor, cssOutlineoffset, cssOutlinestyle, cssOutlinewidth, cssOverflow, cssOverflowwrap, cssOverflowx, cssOverflowy, cssPadding, cssPaddingbottom, cssPaddingleft, cssPaddingright, cssPaddingtop, cssPagebreakafter, cssPagebreakbefore, cssPagebreakinside, cssPerspective, cssPerspectiveorigin, cssPosition, cssQuotes, cssResize, cssRight, cssTabsize, cssTablelayout, cssTextalign, cssTextalignlast, cssTextcombineupright, cssTextdecorationcolor, cssTextdecoration, cssTextdecorationline, cssTextdecorationstyle, cssTextindent, cssTextjustify, cssTextorientation, cssTextoverflow, cssTextshadow, cssTexttransform, cssTextunderlineposition, cssTop, cssTransform, cssTransformorigin, cssTransformstyle, cssTransition, cssTransitiondelay, cssTransitionduration, cssTransitionproperty, cssTransitiontimingfunction, cssUnicodebidi, cssUserselect, cssVerticalalign, cssVisibility, cssWhitespace, cssWidows, cssWidth, cssWordbreak, cssWordspacing, cssWordwrap, cssZindex];
 
+// Allowed Characters array
+allowedChar= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","-"];
+
 // Performance comment arrays
 const kudos = ["Bravo","Well, that for sure looked easy", "Easy enough, huh?","GRrrreat","That one was a no-brainer...","Really?","Think you can do this again?","You are beating the Matrix!","Achievement unlocked: GOD LEVEL!!","Wow, you have to be a TA, aren't you?","...Parker, is it you?"]
 const sucker = ["Oops, sorry about that", "Hmm... that one was not obvious, try again!","I am sending you a picture of a kitty, for your trouble...","Were you paying attention?","Ok, wanna cry?","Ready to give up?","You have to be good at something else, right...?","Really?", "I expected better. I am soooo disappointed","Should I dumb it down for you?","Here is a hing: the next one is not something you know. Because, well, you don't know much obviously..."]
@@ -344,29 +347,30 @@ let randomHtml = htmlArray[Math.floor(Math.random()*htmlArray.length)];
 let randomCss = cssArray[Math.floor(Math.random()*cssArray.length)];
 
 // show me the object
-console.log("This is the word to guess:" + randomHtml.name);
-console.log("----------------");
+// console.log("This is the word to guess: " + randomHtml.name);
+// console.log("----------------");
 
 // show me the name (aka: the word to guess)
 let riddle = randomHtml.name;
-console.log("This is the length of the word to guess: " + riddle.length);
-console.log("----------------");
+// console.log("This is the length of the word to guess: " + riddle.length);
+// console.log("----------------");
 
 
 // Text on main page 
 let w3Link = "click this <" + randomHtml.url + ">link</a> for more information";
 let maskedWord = Array(riddle.length + 1).join(" _ ");
-console.log("This is how the word should display: " + maskedWord);
-console.log("----------------");
+// console.log("This is how the word should display: " + maskedWord);
+// console.log("----------------");
 
 
 // splitting the word to guess into characters in an array
 let wordToGuess = riddle.split("");
-console.log("Word to guess letter by letter (as an array)= " + wordToGuess);
-console.log("----------------");
+// console.log("Word to guess letter by letter (as an array)= " + wordToGuess);
+// console.log("----------------");
 
-var attemptedGuesses = [];
+var allLegitKeysPressed = [];
 var keyName;
+var tallyGuesses;
 
 // number guesses allowed assigned to variable numGuesses (if word too small: less guesses)
 var numGuesses;
@@ -380,37 +384,67 @@ else {
 // // number guesses allowed (same for every word to guess)
 // var numGuesses = riddle.length + 2;
 
-console.log("Number of guesses allowed: " + numGuesses)
-console.log("-------------------")
+// console.log("Number of guesses allowed: " + numGuesses)
+// console.log("-------------------")
+document.querySelector("#word_to_guess").innerHTML= maskedWord
+document.querySelector("#element_definition").innerHTML= randomHtml.definition
 
 // the function happens too early
 document.onkeyup = function(event) {
-    var keyName = event.key;    
-    console.log(keyName);
-    attemptedGuesses.push(keyName);
-}
-document.onkeyup = function(event) {
-    keyName = String.fromCharCode(event.which).toLowerCase();
-    console.log(keyName);
-}
-console.log("Letter guessed in the global scope: " + keyName)
-
-
-console.log("this is the attempted guesses: " + attemptedGuesses);
-console.log("----------------");
-
-
-
-
-// Content to populate on main page
-document.querySelector("#word_to_guess").innerHTML= maskedWord
-document.querySelector("#element_name").innerHTML= riddle
-document.querySelector("#element_definition").innerHTML= randomHtml.definition
-document.querySelector("#element_url").innerHTML= w3Link    
-document.querySelector("#element_type").innerHTML= randomHtml.type    
-document.querySelector("#element_example").innerHTML= randomHtml.example
-document.querySelector("#num_guesses").innerHTML= numGuesses
-document.querySelector("#guesses").innerHTML= keyName
+    keyName = event.key;
+    // including the function here allows it to run when key is pressed!!
+    checkGuessValidity();    
+    // checkDuplicate();
+    // console.log("value in the local scope " + keyName);
+    // Content to populate on main page
+    document.querySelector("#element_name").innerHTML= riddle
+    document.querySelector("#element_type").innerHTML= randomHtml.type    
+    document.querySelector("#element_example").innerHTML= randomHtml.example
+    document.querySelector("#element_url").innerHTML= w3Link    
+    document.querySelector("#num_guesses").innerHTML= numGuesses
+    document.querySelector("#guesses").innerHTML= allLegitKeysPressed
 // document.querySelector("#l_bracket").innerHTML= lBracket
 // document.querySelector("#r_bracket").innerHTML= rBracket
+
+
+}
+// console.log("value in the global scope " + keyName);
+// console.log("----------------");
+
+// console.log("Letter guessed in the global scope: " + keyName)
+// console.log("----------------");
+
+
+// console.log("this is the attempted guesses: " + attemptedGuesses);
+// console.log("----------------");
+
+
+
+var validChar;
+var attemptedGuesses= [];
+function checkGuessValidity() {
+    for (var j = 0; j<allowedChar.length; j++) {
+        if (keyName === allowedChar[j]) {
+            console.log(keyName + " is a valid character")
+            validChar = keyName;
+            allLegitKeysPressed.push(validChar);
+            checkDuplicate();
+        }
+    }
+} 
+
+
+var newKey;
+function checkDuplicate() {
+    for (r = 0; r < (allLegitKeysPressed.length+1); r++) {
+        if (validChar !== allLegitKeysPressed[r]) {
+            newKey = validChar;
+            attemptedGuesses.push(newKey);
+            console.log("AllLegiKeysPressed: " + allLegitKeysPressed);
+            console.log("attemptedGuesses: " + attemptedGuesses);
+            break;
+        } 
+    }
+}
+
 
